92dbff3ec6c9f9e5b55cf6b6d23da44cddba01f6
Antossshka <anton_avdonkin@mail.ru>
2021-11-28 20:01:26 +0300

    Создал отдельные классы для квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
@@ -8,23 +8,15 @@ public class MyFirstProgram1 {
 		hello("User");
 		hello("World");
 
-		double l = 5;
-		double a = 4;
-		double b = 6;
+		Square s = new Square(5);
+		Rectangle r = new Rectangle(4, 6);
 
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 	}
 
-	public static double area(double len){
-		return len * len;
-	}
-
-	public static double area(double a, double b){
-		return a * b;
-	}
 }
