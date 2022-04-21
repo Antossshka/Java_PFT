516c0d784d4b61f3bf95bada21faf12f23a2c39d
Antossshka <anton_avdonkin@mail.ru>
2022-04-22 00:01:32 +0300

    Добавил сравнение списка групп целиком для теста удаления групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -20,6 +20,9 @@ public class GroupDeletionTests extends TestBase{
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
     app.getSessionHelper().logout();
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 
 }
