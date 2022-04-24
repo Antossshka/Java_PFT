d7e02f6e0ec96820be452c1a50b67c07854985e0
Antossshka <anton_avdonkin@mail.ru>
2022-04-24 20:25:50 +0300

    Доработка теста для модификации групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,18 +3,53 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+  private final String id;
   private final String name;
   private final String header;
   private final String footer;
 
-  public GroupData(String name, String header, String footer) {
+  public GroupData(String id, String name, String header, String footer) {
+    this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
+  public GroupData(String name, String header, String footer) {
+    this.id = null;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public String getId() {
+    return id;
+  }
+
   public String getName() {
     return name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData data = (GroupData) o;
+    return Objects.equals(id, data.id) &&
+            Objects.equals(name, data.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name);
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
   }
 
   public String getHeader() {
@@ -25,23 +60,4 @@ public class GroupData {
     return footer;
   }
 
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData data = (GroupData) o;
-    return Objects.equals(name, data.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name);
-  }
 }
