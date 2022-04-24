25c90a6d1ae04c99b560fe005147934653fa3a16
Antossshka <anton_avdonkin@mail.ru>
2022-04-24 23:58:14 +0300

    Добавил сортировку списков для тестов для удаления и модификации групп контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -22,7 +22,10 @@ public class GroupCreationTests extends TestBase {
 
     group.setID(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    Comparator<? super GroupData> byID = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byID);
+    after.sort(byID);
+    Assert.assertEquals(before, after);
   }
 
 }
