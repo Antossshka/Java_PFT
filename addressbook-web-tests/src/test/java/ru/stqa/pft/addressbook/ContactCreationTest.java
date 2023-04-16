9cb3b7ebff7399edf7d7dfc7559a5bba8c2800db
Antossshka <anton_avdonkin@mail.ru>
2023-04-16 23:25:12 +0300

    Мелкая правка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -5,10 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
-
-import static org.testng.Assert.fail;
 
 public class ContactCreationTest {
   private WebDriver wd;
@@ -32,7 +29,7 @@ public class ContactCreationTest {
   @Test
   public void testContactCreation() throws Exception {
     initContactCreation();
-    fillContactForm(new ContactData("Dmitry", "Blinovsky", "Petrob", "89991112299", "petrov.db@mail.ru", "Mowcow"));
+    fillContactForm(new ContactData("Dmitry", "Blinovsky", "Petrov", "89991112299", "petrov.db@mail.ru", "Mowcow"));
     submitContactCreation();
     returnToHomePage();
   }
