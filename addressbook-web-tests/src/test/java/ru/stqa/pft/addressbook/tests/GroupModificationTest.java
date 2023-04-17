c0c3d5a47964d7741a2e9120e0f3782b9b59394e
Antossshka <anton_avdonkin@mail.ru>
2023-04-18 00:22:53 +0300

    Добавил тест для модификации групп

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTest extends TestBase{
+
+  @Test
+  public void testGroupModification() {
+    app.navigationHelper().gotoGroupPage();
+    app.groupHelper().selectGroup();
+    app.groupHelper().initGroupModification();
+    app.groupHelper().fillGroupForm(new GroupData("test12", "test23", "test34"));
+    app.groupHelper().submitGroupModification();
+    app.groupHelper().returnToGroupPage();
+  }
+}
