516fea184109c0199299b0964e3ba24f47f8c4c3
Antossshka <anton_avdonkin@mail.ru>
2023-04-23 17:36:25 +0300

    .............

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -13,18 +13,7 @@ public class Equation {
 
     double d = b * b - 4 * a * c;
 
-    if (a == 0) {
-      if (b == 0) {
-        if (c == 0) {
-          n = -1;
-        } else {
-          n = 0;
-        }
-      } else {
-        n = 1;
-      }
-    } else {
-
+    if (a != 0) {
       if (d > 0) {
         n = 2;
       } else if (d == 0) {
@@ -33,7 +22,18 @@ public class Equation {
         n = 0;
       }
     }
+
+    else if (b != 0) {
+      n = 1;
+
+    } else if (c != 0) {
+      n = 0;
+
+    } else {
+      n = -1;
+    }
   }
+
   public int rootNumber() {
     return n;
   }
