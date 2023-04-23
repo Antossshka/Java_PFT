c93804688acdad4e0c23515e32583427a17aa359
Antossshka <anton_avdonkin@mail.ru>
2023-04-23 17:50:55 +0300

    Добавил выбор браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,18 +3,34 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 
 import java.time.Duration;
 
 public class ApplicationManager {
+  private final String browser;
   WebDriver wd;
   private ContactHelper contactHelper;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
 
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
+
   public void init() {
-    wd = new ChromeDriver();
+    String browser = Browser.CHROME.browserName();
+    if (browser == Browser.CHROME.browserName()) {
+      wd = new ChromeDriver();
+    } else if (browser == Browser.EDGE.browserName()) {
+      wd = new EdgeDriver();
+    } else if (browser == Browser.FIREFOX.browserName()) {
+      wd = new FirefoxDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
