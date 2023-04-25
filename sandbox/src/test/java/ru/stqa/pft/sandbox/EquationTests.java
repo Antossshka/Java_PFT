58f01aa99ebce0d7fc9b3dacad1305db168435db
Antossshka <anton_avdonkin@mail.ru>
2023-04-25 19:19:43 +0300

    Лекция 3.2

--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class EquationTest {
+public class EquationTests {
 
   @Test
   public void test0() {
@@ -21,5 +21,23 @@ public class EquationTest {
   public void test2() {
     Equation e = new Equation(1, 5,6);
     Assert.assertEquals(e.rootNumber(), 2);
+  }
+
+  @Test
+  public void testLinear() {
+    Equation e = new Equation(0, 1,1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+
+  @Test
+  public void testConstant() {
+    Equation e = new Equation(0, 0,1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+
+  @Test
+  public void testZero() {
+    Equation e = new Equation(0, 0,0);
+    Assert.assertEquals(e.rootNumber(), -1);
   }
 }
