12e4930ff35aa0fd81e05b1bc2f56d951d48bc28
Antossshka <anton_avdonkin@mail.ru>
2023-04-25 19:35:05 +0300

    3.3 Запуск тестов в различных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 
 import java.time.Duration;
 
@@ -12,9 +15,20 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new ChromeDriver();
+    if (browser.equals(Browser.CHROME.browserName())) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(Browser.EDGE.browserName())) {
+      wd = new EdgeDriver();
+    } else if (browser.equals(Browser.FIREFOX.browserName())) {
+      wd = new FirefoxDriver();
+    }
     wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
