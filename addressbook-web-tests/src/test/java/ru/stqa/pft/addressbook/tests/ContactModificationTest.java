afbd7143bed50445b6a19e8304918fcba9d5628b
Antossshka <anton_avdonkin@mail.ru>
2023-05-06 19:15:21 +0300

    Update ContactModificationTest.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactModificationTest extends TestBase{
 
@@ -10,11 +9,11 @@ public class ContactModificationTest extends TestBase{
   public void testContactModification() {
     app.navigationHelper().gotoHomePage();
     if (! app.contactHelper().isThereAContact()) {
-      app.contactHelper().createContact(new ContactData("Dmitry", "Blinovsky", "Petrov", "89991112299", "petrov.db@mail.ru", "Mowcow", "test1"));
+      app.contactHelper().createContact(new ContactData("Dmitry", "Blinovsky", "Petrov", "89991112299", "petrov.db@mail.ru", "Moscow", "test1"));
     }
     app.contactHelper().selectContact();
     app.contactHelper().initContactModification();
-    app.contactHelper().fillContactForm(new ContactData("Dmitry1", "Blinovsky2", "Petrov3", "89991112299", "petrov.db@mail.ru", "Mowcow", null), false);
+    app.contactHelper().fillContactForm(new ContactData("Dmitry1", "Blinovsky2", "Petrov3", "89991112299", "petrov.db@mail.ru", "Moscow", null), false);
     app.contactHelper().submitContactModification();
     app.contactHelper().returnToHomePage();
   }
