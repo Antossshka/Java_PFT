b6b5f3849104bd620ce10fc84905ab362ff7090d
Antossshka <anton_avdonkin@mail.ru>
2023-05-14 02:06:29 +0300

    Реструктуризация

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -1,11 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import java.util.List;
 
 public class GroupDeletionTest extends TestBase{
+
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.navigationHelper().gotoGroupPage();
+    if (! app.groupHelper().isThereAGroup()) {
+      app.groupHelper().createGroup(new GroupData("test1", null, null));
+    }
+  }
 
   @Test
   public void testGroupDeletion() throws Exception {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,27 +9,30 @@ import java.util.*;
 
 public class GroupModificationTest extends TestBase{
 
-  @Test
-  public void testGroupModification() {
+  @BeforeMethod
+  public void ensurePreconditions() {
     app.navigationHelper().gotoGroupPage();
     if (! app.groupHelper().isThereAGroup()) {
       app.groupHelper().createGroup(new GroupData("test1", null, null));
     }
+  }
+
+
+  @Test
+  public void testGroupModification() {
     List<GroupData> before = app.groupHelper().getGroupList();
-    app.groupHelper().selectGroup(before.size() - 1);
-    app.groupHelper().initGroupModification();
-    GroupData group = new GroupData("test1", "test2", "test3", before.get(before.size() - 1).getId());
-    app.groupHelper().fillGroupForm(group);
-    app.groupHelper().submitGroupModification();
-    app.groupHelper().returnToGroupPage();
+    int index = before.size() - 1;
+    GroupData group = new GroupData("test1", "test2", "test3", before.get(index).getId());
+    app.groupHelper().modifyGroup(index, group);
     List<GroupData> after = app.groupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
 
-    before.remove(before.size() - 1);
+    before.remove(index);
     before.add(group);
     Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
     before.sort(byId);
     after.sort(byId);
     Assert.assertEquals(before, after);
   }
+
 }
